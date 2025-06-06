d4dbd6c72331ba9c44a6ff5260f1f04e90be90b1
happywwyy <2196185429@qq.com>
2025-06-06 17:07:29 +0800

    prototype

---
+++ js/prototype/1.java
@@ -0,0 +1,13 @@
+// 定义Puppy类
+public class Puppy {
+  // 成员变量
+  int pupyAge;
+  public class Puppy(age) {
+    // This constructor has one parameter，
+    pupyAge = age;
+  }
+  // 公有方法
+  public void say() {
+    System.out.println("汪汪")
+  }
+}
