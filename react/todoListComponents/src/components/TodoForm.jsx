85f0015333e1f51ed2c7cab56e5f5cb6f685f5c7
happywwyy <2196185429@qq.com>
2025-06-07 17:10:36 +0800

    todoListComponents

--- react/todoListComponents/src/components/TodoForm.jsx
+++ react/todoListComponents/src/components/TodoForm.jsx
@@ -1,7 +1,29 @@
-function TodoForm({ addTodo }) {
-    return (
-      <form>
+import { useState } from 'react'
+function TodoForm({ props }) {
+    const onAdd = props.onAdd
+    const [text, setText] = useState('')
 
+    const handleSubmit = (e) => {
+        // 阻止默认行为
+        // 由js 来控制
+        e.preventDefault() // event api
+        // console.log(text) // e.target.value
+        noAdd(text)
+        // todos？ 打报告
+    }
+
+    const handleChange = (e) => {
+        setText(e.target.value)
+    }
+    return (
+      <form action="http://www.baidu.com" onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          placeholder="请输入待办事项" 
+          value={text}
+          onChange={handleChange}
+        />
+        <button>添加</button>
       </form>
     )
 }
