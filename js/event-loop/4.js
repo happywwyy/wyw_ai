bc949d469684624ec73ba49ed9961a3c0dd41090
happywwyy <2196185429@qq.com>
2025-07-11 17:04:55 +0800

    event-loop

---
+++ js/event-loop/4.js
@@ -0,0 +1,17 @@
+console.log('同步Start')
+const promise1 = Promise.resolve('First Promise')
+const promise2 = Promise.resolve('Secend Promise')
+const promise3 = new Promise(resolve => {
+  console.log('Promise3')
+  resolve('Third Promise')
+})
+promise1.then(value => console.log(value))
+promise2.then(value => console.log(value))
+promise3.then(value => console.log(value))
+setTimeout(() => {
+  console.log('下一把再见')
+}, 0)
+promise1.then(value => console.log(value))
+promise2.then(value => console.log(value))
+promise3.then(value => console.log(value))
+console.log('同步end')
