f00ddda8a8ceffc3be57d382a7effefa11a442cd
happywwyy <2196185429@qq.com>
2025-07-12 10:06:10 +0800

    router-demo

---
+++ react/router-demo/src/App.jsx
@@ -0,0 +1,25 @@
+import {
+  BrowserRouter as Router, // 前端路由
+  Routes, // 路由设置容器
+  Route // 单条路由
+} from 'react-router-dom'
+import { useState } from 'react'
+import './App.css'
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  return (
+    <>
+      {/* 前端路由接管一切，配置 */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/About" element={<About />}/>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
