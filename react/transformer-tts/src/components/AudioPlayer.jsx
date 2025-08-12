19211c8d23ae23977f140a4c0e93ce9acde32a34
happywwyy <2196185429@qq.com>
2025-08-12 21:36:07 +0800

    端模型 文生视频

---
+++ react/transformer-tts/src/components/AudioPlayer.jsx
@@ -0,0 +1,34 @@
+import { use } from 'react';
+import {
+    useEffect,
+    useRef,
+} from 'react';
+
+const AudioPlayer = ({audioUrl,mimeType}) => { 
+    const audioPlayer = useRef(null);
+    const audioSource = useRef(null);
+    useEffect(() => { 
+        if(audioPlayer.current && audioSource.current){
+            audioSource.current.src = audioUrl;
+        }
+    }, [audioUrl]);
+
+    return (
+        <>
+            <div className="flex relative z-10 my-4 w-full">
+                <audio 
+                    src={audioUrl}
+                    ref={audioPlayer}
+                    controls
+                    className='w-full h-14 rounded-lg bg-white shadow-xl shadow-black/5 ring-1 ring-slate-700/10'
+                >
+                    {/* 浏览器兼容声音的类型不一样，多个source */}
+                    <source ref={audioSource} type={mimeType} />
+                </audio>
+            </div>
+        </>
+    )
+}
+
+
+export default AudioPlayer;
